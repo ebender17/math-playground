f67420ef8fd6dc31063cfa7036fcb9d0b5da54b9
Emily Bender <bender.emily1@gmail.com>
2024-01-15 14:04:04 -0500

    simple radial trigger

---
+++ Assets/Scripts/RadialTrigger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class RadialTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private float radius = 1;
+    [SerializeField]
+    private Transform player = null;
+
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Vector3 center = transform.position;
+        
+        if (player == null)
+            return;
+
+        Vector3 playerPosition = player.transform.position;
+        Vector3 delta = center - playerPosition;
+        float sqrDistance = delta.x * delta.x + delta.y * delta.y + delta.z * delta.z;
+        bool inside = sqrDistance <= radius * radius;
+
+        Gizmos.color = inside ? Color.white : Color.red;
+        Gizmos.DrawSphere(center, radius);
+    }
+#endif
+
+}
